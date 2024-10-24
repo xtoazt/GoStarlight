e8bcdbd9853b5c303edecdafe2d41770eb985777
infamy <162852023+enterheadlines@users.noreply.github.com>
2024-10-24 21:07:43 +0900

    asdasdwasdwasd

--- general/js/cloak.js
+++ general/js/cloak.js
@@ -39,3 +39,44 @@ if (url) {
     }
   };
 }
+
+var url = window.location.href; // Get the current URL
+
+if (url) {
+  var win;
+
+  document.getElementById('openscriptButton').onclick = function() {
+    if (win) {
+      win.focus(); // Focus on the existing window if it is already open
+    } else {
+      win = window.open(); // Open a new window
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+
+      // Set the title and favicon
+      win.document.title = "Google";
+      var link = win.document.createElement('link');
+      link.rel = 'icon';
+      link.href = 'https://starlight-phi.vercel.app/img/images/icons/google.ico'; // Google favicon
+      win.document.head.appendChild(link);
+
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url; // Set the iframe source to the current URL
+      win.document.body.appendChild(iframe);
+
+      // Redirect the current window to Google
+      window.location.replace("https://google.com");
+
+      var interval = setInterval(function() {
+        if (win.closed) {
+          clearInterval(interval);
+          win = undefined; // Reset the win variable when the window is closed
+        }
+      }, 500);
+    }
+  };
+}
--- general/js/settings.js
+++ general/js/settings.js
@@ -83,26 +83,4 @@ document.getElementById('popupToggle').addEventListener('change', function () {
       document.getElementById('logoUrl').value = savedLogoUrl;
       document.querySelector('link[rel="shortcut icon"]').href = savedLogoUrl;
     }
-    document.getElementById('popupToggle').checked = popupEnabled;
-
-    if (popupEnabled && window.self === window.top) {
-      const popup = open("about:blank", "_blank");
-      const doc = popup.document;
-      const iframe = doc.createElement("iframe");
-      const style = iframe.style;
-      const link = doc.createElement("link");
-      const name = "Google";
-      const icon = "/img/images/icons/google.ico";
-      doc.title = name;
-      link.rel = "icon";
-      link.href = icon;
-      iframe.src = location.href;
-      style.position = "fixed";
-      style.top = style.bottom = style.left = style.right = 0;
-      style.border = style.outline = "none";
-      style.width = style.height = "100%";
-      doc.head.appendChild(link);
-      doc.body.appendChild(iframe);
-      location.replace(getRandomSite());
-    }
-  });
+   

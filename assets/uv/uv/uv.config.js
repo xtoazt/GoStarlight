de73b11991fc946e6f662c99d36545f5a39a6771
infamy <162852023+enterheadlines@users.noreply.github.com>
2025-01-07 18:18:05 +0900

    c

--- assets/uv/uv/uv.config.js
+++ assets/uv/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/assets/uv/',
-    bare:'https://bare.benrogo.net/',
+    bare:'https://bareglsdacier.global.ssl.fastly.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/assets/uv/uv/uv.handler.js',
